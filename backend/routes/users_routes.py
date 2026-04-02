eadd5867075eaae8722474526b9caa27d090499e
suhas <suhas8838@gmail.com>
2026-04-02 21:28:58 +0530

    fix: modified user apis

--- backend/routes/users_routes.py
+++ backend/routes/users_routes.py
@@ -1,11 +1,20 @@
-from fastapi import APIRouter, Request
-from services.rate_limiter import limiter
+from fastapi import APIRouter, Request, Depends
 from loguru import logger
+from sqlalchemy.orm import Session, load_only
+
+from services.rate_limiter import limiter
+from core.db import get_db
+from models.db_models import User
+from utils.response_generator import success_response, error_response
 
 user_router = APIRouter(tags=['Users'])
 
 
 @user_router.get("/")
 @limiter.limit("5/minute")
-async def get_users(request: Request):
-    return {"users": ["Suhas", "Admin"]}
+async def get_users(request: Request, db: Session = Depends(get_db)):
+    users = db.query(User).options(load_only(User.id, User.name, User.email, User.role, User.is_active)).all()
+    return success_response(
+        data=users,
+        message='All users are fetched',
+    )
